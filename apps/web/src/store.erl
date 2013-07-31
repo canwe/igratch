e093f444ae25e598ad0b707983ccdde1f55388a0
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-07-31 12:09:22 +0000

    merge products and store

--- apps/web/src/store.erl
+++ apps/web/src/store.erl
@@ -1,18 +1,33 @@
 -module(store).
 -compile(export_all).
 -include_lib("n2o/include/wf.hrl").
+-include_lib("kvs/include/products.hrl").
+
 -include("records.hrl").
 
-main() -> #dtl{file="dev", bindings=[{title,<<"Store">>},{body, body()}]}.
+-define(PAGE_SIZE, case wf:session(page_size) of list -> 2; _ -> 8 end).
+
+main() -> wf:session(page_size, list), #dtl{file="dev", bindings=[{title,<<"Store">>},{body, body()}]}.
 
 body() -> index:header() ++[
   #section{class=[section, "main-no-slider"], body=[
     #panel{class=[container], body=[
       #panel{class=["row-fluid"], body=[
-        #table{id=products, class=[table, "table-hover"], body=[[#product_row{product=P} || P <- kvs:all(product)]] }
-      ]}
+        #table{id=products, class=[table, "table-hover"], body=[list_products(1)] }
+      ]},
+      #panel{class=[pagination, "pagination-large","pagination-centered"],body=[ #list{id=pagination, body=pagination(1)} ]}
     ]}
-  ]} ] ++index:footer().
+  ]},
+  #section{class=[section, alt], body=#panel{class=[container], body=[
+      #panel{class=["hero-unit", "text-center"], body=[
+        #h1{body= <<"Got a question?">>},
+        #p{body= <<"want to work with us to move your bussines to the next level? Well, dont be afraid">>},
+        #link{class=[btn, "btn-large", "btn-info"], body= <<"contact us">>}
+      ]}
+  ]}}
+  ] ++index:footer().
+
+list_products(Page) -> [#product_row{product=P} || P <- lists:sublist(kvs:all(product), (Page-1) * ?PAGE_SIZE + 1, ?PAGE_SIZE)].
 
 body1()-> index:header() ++ [
   #panel{id="main-container", class=["container-fluid", "main-no-slider"], body=[
@@ -65,6 +80,28 @@ body1()-> index:header() ++ [
     ]}
   ]} ] ++ index:footer().
 
+pagination(Page)->
+  PageCount = (length(kvs:all(product))-1) div ?PAGE_SIZE + 1,
+  error_logger:info_msg("Page: ~p", [PageCount]),
+  [
+  #li{class=[if Page==1-> "disabled"; true->[] end, "previous"], body=#link{body=#i{class=["icon-circle-arrow-left", "icon-large"]}, postback={page, 1} }},
+  [#li{class=if I==Page -> active;true->[] end,body=#link{id="pglink"++integer_to_list(I),body=#span{style="line-height:normal;", body=integer_to_list(I)}, postback={page, I} }} 
+    || I <- lists:seq(1, PageCount)],
+  #li{class=[if PageCount==Page -> "disabled";true->[] end,"next"], body=#link{body=#i{class=["icon-circle-arrow-right", "icon-large"]}, postback={page, PageCount}}}
+  ].
+
 event(init) -> [];
 event({product_feed, Id})-> wf:redirect("/product?id=" ++ integer_to_list(Id));
+event({page, Page})->
+  wf:update(pagination, pagination(Page)),
+  wf:update(products, wf:js_escape(wf:render(list_products(Page))));
+event({product, Id})-> wf:redirect("/product?id=" ++ Id);
+event(to_list)->
+  wf:session(page_size, list),
+  wf:update(products, wf:js_escape(list_products(1))),
+  wf:update(pagination, pagination(1));
+event(to_grid)->
+  wf:session(page_size, grid),
+  wf:update(products, list_products(1)),
+  wf:update(pagination, pagination(1));
 event(Event) -> error_logger:info_msg("Page event: ~p", [Event]), ok.
