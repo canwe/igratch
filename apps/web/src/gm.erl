21f6add002fda9347c4d72d346cdba96b54276f6
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-08-07 06:19:28 +0000

    process files with spaces

--- apps/web/src/gm.erl
+++ apps/web/src/gm.erl
@@ -6,9 +6,9 @@
 %
 make_thumb(Input, Width, Height, Output) ->
   W = integer_to_list(Width), H = integer_to_list(Height),
-  Cmd = lists:concat(["gm convert ", Input,
+  Cmd = lists:concat(["gm convert \"", Input, "\"",
     " -background transparent -gravity center",
     " -extent ", W, "x", H,
     " -resize ", W, "x", H,
-    " +profile \"*\" ", Output]),
-  case os:cmd(Cmd) of [] -> ok; Error -> {error, Error} end.
+    " +profile \"*\" \"", Output, "\""]),
+  case os:cmd(Cmd) of [] -> ok; Error -> error_logger:info_msg("No thumb. ~p failed.", [Cmd]),{error, Error} end.
