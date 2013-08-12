74ccbfde43505015a8433df55469b1dc1c49d586
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-08-12 18:06:53 +0000

    remove dubicates for all categories

--- apps/web/src/store.erl
+++ apps/web/src/store.erl
@@ -25,9 +25,7 @@ body()->
       ]},
       #panel{class=["row-fluid"], body=[
         #panel{class=[span9, "tab-content"], body=[
-          #panel{id=all, class=["tab-pane", active], body=[
-            [[#product_entry{entry=E, mode=line, category=Name} || E <- kvs_feed:entries(lists:keyfind(products,1,Feeds), undefined, ?PAGE_SIZE)] || #group{feeds=Feeds, name=Name} <- kvs:all(group)]
-          ]},
+          #panel{id=all, class=["tab-pane", active], body= all()},
           [ begin
               {Feed,Fid} = lists:keyfind(products,1,Feeds),
               Entries = kvs_feed:entries({Feed,Fid}, undefined, ?PAGE_SIZE),
@@ -55,57 +53,12 @@ body()->
   ]}}
   ] ++ index:footer().
 
-
-body1()-> index:header() ++ [
-  #panel{id="main-container", class=["container-fluid", "main-no-slider"], body=[
-    #panel{id=first, body=[
-      #panel{class=["row-fluid", "store-item"], body=[
-        #panel{class=["store-item-inner", clearfix], body=[
-          #panel{class=[breadcrumbs], body=[
-            #h3{class=[capital, blue], body=[<<"Action">>, #span{class=["title"], body= <<"Crysis 3">>} ]}
-          ]},
-          #panel{class=["no-margin-left","big-shadow"], body=[
-            #image{class=[border], alt= <<"Store Image">>, image= <<"/static/img/store-img1.jpg">>}
-          ]},
-          #panel{class=[span10, offset1], body=[
-            #panel{class=["game-info"], body=[
-              #p{body=[
-                <<"Developer: ">>,
-                #span{class=["blue"], body= <<"Electronic Arts">>},
-                #image{class=["pull-right"], alt= <<"Rating">>, image= <<"/static/img/rating.png">>}
-              ]},
-              #p{class=["store-item-text"], body= <<"The award-winning developer Crytek is back with Crysis 3, the first blockbuster shooter of 2013! Return to the fight as Prophet, the Nanosuit soldier on a quest to rediscover his humanity and exact brutal revenge. Adapt on the fly with the stealth and armor abilities of your unique Nanosuit as you battle through the seven wonders of New York’s Liberty Dome. Unleash the firepower of your all-new, high-tech bow and alien weaponry to hunt both human and alien enemies. And uncover the truth behind the death of your squad while reestablishing the power of human will in a rich story full of exciting twists and turns. Crysis 3 is the ultimate sandbox shooter, realised in the stunning visuals only Crytek and the latest version of CryENGINE can deliver. Assess Adapt, and Attack starting spring 2013 on Xbox 360, PlayStation 3, and PC.">>}
-            ]},
-            #panel{class=["buy-review", span3, offset5, "center-wider"], body=[
-              #link{url= <<"#">>, class=[btn, "btn-orange capital"], body= <<"Buy it!">>},
-              #link{url= <<"review.html">>, class=[btn, "btn-blue", capital], body= <<"Review">>}
-            ]}
-          ]}
-        ]},
-        #panel{class=["row-fluid", "store-item"], body=[
-          #panel{class=["store-item-inner", clearfix], body=[
-            #panel{class=[breadcrumbs], body=[
-              #h3{class=[capital, blue], body=[<<"Action">>, #span{class=["title"], body= <<"Battlefield 4">>}]}
-            ]},
-            #panel{class=["no-margin-left","big-shadow"], body=[
-              #image{class=[border], alt= <<"Store Image">>, image= <<"/static/img/store-img2.jpg">>}
-            ]},
-            #panel{class=[span10, offset1], body=[
-              #panel{class=["game-info"], body=[
-                #p{body=[<<"Developer: ">>, #span{class=[blue], body= <<"Electronic Arts">>}]},
-                #image{class=["pull-right"], alt= <<"Rating">>, image= <<"/static/img/rating.png">>}
-              ]},
-              #p{class=["store-item-text"], body= <<"The award-winning developer Crytek is back with Crysis 3, the first blockbuster shooter of 2013! Return to the fight as Prophet, the Nanosuit soldier on a quest to rediscover his humanity and exact brutal revenge. Adapt on the fly with the stealth and armor abilities of your unique Nanosuit as you battle through the seven wonders of New York’s Liberty Dome. Unleash the firepower of your all-new, high-tech bow and alien weaponry to hunt both human and alien enemies. And uncover the truth behind the death of your squad while reestablishing the power of human will in a rich story full of exciting twists and turns. Crysis 3 is the ultimate sandbox shooter, realised in the stunning visuals only Crytek and the latest version of CryENGINE can deliver. Assess Adapt, and Attack starting spring 2013 on Xbox 360, PlayStation 3, and PC.">>}
-            ]},
-            #panel{class=["buy-review", span3, offset5, "center-wider"], body=[
-              #link{url= <<"#">>, class=[btn, "btn-orange", capital], body= <<"Buy it!">>},
-              #link{url= <<"review.html">>, class=[btn, "btn-blue", capital], body= <<"Review">>}
-            ]}
-          ]}
-        ]}
-      ]}
-    ]}
-  ]} ] ++ index:footer().
+all() -> [
+  #product_entry{entry=E} || E <-  lists:foldl(
+    fun(#entry{entry_id=Eid}=E, Ai) -> [E|lists:filter(fun(#entry{entry_id=Eid1})-> Eid =/= Eid1 end, Ai)] end,
+    [],
+    lists:flatten([ [E || E <- kvs_feed:entries(Feed, undefined, ?PAGE_SIZE)]
+      || Feed <- [case lists:keyfind(products, 1, F) of false-> []; Fd -> Fd end || #group{feeds=F}<- kvs:all(group)] ]))].
 
 event(init) -> wf:reg(?MAIN_CH),[];
 event({delivery, [_|Route], Msg}) -> process_delivery(Route, Msg);
