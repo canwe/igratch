133a97fa37de44e7b9cf1e9fcceeec1d4ba3c01e
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-08-19 16:00:05 +0300

    check features to show dashboard menu

--- apps/web/src/dashboard.erl
+++ apps/web/src/dashboard.erl
@@ -4,13 +4,20 @@
 -include_lib("kvs/include/users.hrl").
 
 sidebar_menu(Who, What, Page, Sublist) ->
-%  error_logger:info_msg("~p look for ~p",[Who, What]),
   List = [
     begin
       {Class,Sub} = if Page ==I -> {active, Sublist} ; true -> {"", []} end,
       [#li{class=Class, body=#link{url="/"++atom_to_list(I), body=T}}, Sub]
     end 
-    || {I, T} <- lists:filter(fun({E,_}) -> if Who==What -> true; true -> E==Page end end, [
+    || {I, T} <- lists:filter(fun({E,_}) ->
+        if Who==What ->
+          case E of
+            myreviews -> kvs_acl:check_access(What#user.email, {feature, reviewer})==allow;
+            mygames -> kvs_acl:check_access(What#user.email, {feature, developer})==allow;
+            admin -> kvs_acl:check_access(What#user.email, {feature, admin})==allow;
+            _ -> true
+          end; true -> E==Page end
+      end, [
       {profile, <<"profile">>},
       {myreviews, <<"reviews">>},
       {mygames, <<"games">>},
