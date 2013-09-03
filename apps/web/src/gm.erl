531512bd0ff45e931f4684173f3e9b09d03a5a7c
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-09-04 02:12:29 +0300

    optimize thumb generation

--- apps/web/src/gm.erl
+++ apps/web/src/gm.erl
@@ -1,16 +1,11 @@
 -module(gm).
 -compile(export_all).
 
-%
-% -flatten  psd layers to png
-%
-make_thumb(Input, Width, Height, Output) ->
-    error_logger:info_msg("Input ~p~n", [Input]),
-    error_logger:info_msg("Output ~p~n", [Output]),
-  W = integer_to_list(Width), H = integer_to_list(Height),
-  Cmd = lists:concat(["gm convert \"", Input, "\"",
-    " -background transparent -gravity center",
-    " -extent ", W, "x", H,
-    " -resize ", W, "x", H,
-    " +profile \"*\" \"", Output, "\""]),
-  case os:cmd(Cmd) of [] -> ok; Error -> error_logger:info_msg("No thumb. ~p failed.", [Cmd]), error_logger:info_msg("ERROR: ~p", [Error]),{error, Error} end.
+make_thumb(In,W,H,Out) ->
+    case os:cmd(lists:concat([
+        "gm convert -size ", integer_to_list(W*2), "x", integer_to_list(H*2),
+        " \"",In,"\"",
+        " -thumbnail ", integer_to_list(W*H), "@",
+        " -gravity center -background transparent",
+        " -extent ",integer_to_list(W),"x",integer_to_list(H),
+        " \"",Out,"\""])) of [] -> ok; E -> error_logger:info_msg("GM:~p", [E]), {error, E} end.
