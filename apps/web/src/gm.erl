5c4e22e877d249e4736062cd475bfa3cd53d51e2
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-09-04 02:55:47 +0300

    return resize extend order

--- apps/web/src/gm.erl
+++ apps/web/src/gm.erl
@@ -5,7 +5,7 @@ make_thumb(In,W,H,Out) ->
     case os:cmd(lists:concat([
         "gm convert -size ", integer_to_list(W*2), "x", integer_to_list(H*2),
         " \"",In,"\"",
-        " -thumbnail ", integer_to_list(W*H), "@",
         " -gravity center -background transparent",
         " -extent ",integer_to_list(W),"x",integer_to_list(H),
+        " -thumbnail ", integer_to_list(W),"x",integer_to_list(H), % " -thumbnail ", integer_to_list(W*H), "@"
         " \"",Out,"\""])) of [] -> ok; E -> error_logger:info_msg("GM:~p", [E]), {error, E} end.
