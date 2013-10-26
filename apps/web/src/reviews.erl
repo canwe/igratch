e7249a5efad91dc3bc2693466cbb8ca9b30fc7c9
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-10-26 05:15:05 +0300

    cut some css

--- apps/web/src/reviews.erl
+++ apps/web/src/reviews.erl
@@ -76,7 +76,7 @@ render_element(#div_entry{entry=#entry{entry_id=Eid}=E, state=#feed_state{view=r
             #h3{body=#span{id=?EN_TITLE(UiId), class=[title], body=
                 #link{style="color:#9b9c9e;", body=E#entry.title, url="/review?id="++wf:to_list(Eid)}}},
 
-            #p{id=?EN_DESC(UiId), body=product_ui:shorten(E#entry.description)},
+            #p{id=?EN_DESC(UiId), body=product_ui:shorten(wf:js_escape(E#entry.description))},
             #panel{id=?EN_TOOL(UiId), class=[more], body=[
                 #link{body=[<<"read more">>], url="/review?id="++wf:to_list(Eid)} ]}]}]);
 
