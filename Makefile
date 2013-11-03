a10bd59a4e62e2555a4acd35c57e5c43d5e832f0
Maxim Sokhatsky <maxim@synrc.com>
2013-11-03 18:54:23 +0200

    otp.mk

---
+++ Makefile
@@ -0,0 +1,31 @@
+RELEASE := igratch
+COOKIE  := node_runner
+APPS    := web amqp_client avz cowboy erlydtl gproc kai kvs lager mimetypes mqs n2o oauth rabbit_common ranch sync feed_server
+ERL_ARGS     := -args_file rels/web/files/vm.args -config rels/web/files/sys.config
+RUN_DIR      ?= ./rels/web/devbox
+LOG_DIR      ?= ./rels/web/devbox/logs
+N2O          := deps/n2o/priv/static
+FILES        := apps/web/priv/static/n2o
+BOOTSTRAP    := apps/web/priv/static/bootstrap
+FONTAWESOME  := apps/web/priv/static/font-awesome
+MCE          := apps/web/priv/static/tinymce
+LESSJS       := apps/web/priv/static/less
+
+default: .applist compile static-link
+
+static-link:
+	rm -rf $(N2O)
+	rm -rf $(FILES)
+	rm -rf $(BOOTSTRAP)
+	rm -rf $(FONTAWESOME)
+	rm -rf $(LESSJS)
+	rm -rf $(MCE)
+	ln -sf ../../n2o_scripts $(N2O)
+	mkdir -p $(shell dirname $(FILES))
+	ln -sf ../../../../deps/n2o/priv/static/tinymce $(MCE)
+	ln -sf ../../../../deps/n2o/priv/static/n2o $(FILES)
+	ln -sf ../../../../deps/n2o/priv/static/bootstrap $(BOOTSTRAP)
+	ln -sf ../../../../deps/n2o/priv/static/font-awesome $(FONTAWESOME)
+	ln -sf ../../../../deps/n2o/priv/static/less $(LESSJS)
+
+include otp.mk
---
+++ otp.mk
@@ -0,0 +1,40 @@
+empty :=
+ROOTS := apps deps
+space := $(empty) $(empty)
+VSN   := $(shell expr substr `git rev-parse HEAD` 1 6)
+DATE  := $(shell git show -s --format="%ci" HEAD | sed -e 's/\+/Z/g' -e 's/-/./g' -e 's/ /-/g' -e 's/:/./g')
+ERL_LIBS := $(subst $(space),:,$(ROOTS))
+PLT_NAME := .dialyzer.plt
+
+test: eunit ct
+compile: get-deps static-link
+get-deps compile clean update-deps:
+	rebar $@
+.applist:
+	./envgen.erl $(APPS) > $@
+$(RUN_DIR) $(LOG_DIR):
+	mkdir -p $(RUN_DIR)
+	mkdir -p $(LOG_DIR)
+console: .applist
+	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval \
+		'[ok = application:ensure_started(A, permanent) || A <- $(shell cat .applist)]'
+start: $(RUN_DIR) $(LOG_DIR) .applist
+	run_erl -daemon $(RUN_DIR)/ $(LOG_DIR)/ "exec $(MAKE) console"
+attach:
+	to_erl $(RUN_DIR)/
+release:
+	relx
+stop:
+	kill -9 `ps ax -o pid= -o command=|grep $(RELEASE)|grep $(COOKIE)|awk '{print $$1}'`
+$(PLT_NAME):
+	ERL_LIBS=$(ERL_LIBS) dialyzer --build_plt --output_plt $(PLT_NAME) --apps $(APPS) 
+dialyze: $(PLT_NAME)
+	dialyzer apps/*/ebin deps/*/ebin --plt $(PLT_NAME) --no_native -Werror_handling -Wunderspecs -Wrace_conditions
+tar:
+	tar zcvf $(RELEASE)-$(VSN)-$(DATE).tar.gz _rel/lib/*/ebin _rel/lib/*/priv _rel/bin _rel/releases
+eunit:
+	rebar eunit skip_deps=true
+ct:
+	rebar ct skip_deps=true verbose=1
+
+.PHONY: get-deps compile clean console start attach release update-deps dialyze
